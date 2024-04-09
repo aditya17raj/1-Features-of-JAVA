64909c176ee053fe6cc51a3c1a679ba0e4ee39ec
Adi Raj <25782313-AdiRaj7@users.noreply.replit.com>
2024-04-09 14:51:46 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+Following are the features of JAVA programming :-
+
+1. OBJECT ORIENTED PROGRAMMING
+  In Java , everything is in the form of an object. Java can be easily extended as it is based on object model.
+
+2. ROBUST
+  Java makes an effort to eliminate error prone codes by emphasizing mainly an compile time error checking and runtime checking. But the main areas which Java improved were Memory Management and the Mishandled exceptions by introducing automatic Garbage Collector and Exception handling.
+
+3. PLATFORM INDEPENDENT
+  Unlike other programming languages, Java is platform independent. It can be run on any platform which has JVM installed.
+
+  On compilation Java program is compiled into bytecode. This bytecode is platform independent and can be run on any machine , plus this bytecode format with Java Runtime enviornment can run Java programs.
+
+4. SECURE
+  When it comes to security, Java is always the first chice. With Java secure features it enable us to develop virus free, temper free programs. 
+
+5. MULTI-THREADING
+  Java supports multithreading. It is a powerful feature which enables us to develop applications which can be easily scaled up. Java multithreading features makes it possible to write program that can do multiple tasks simulaetaneously.
